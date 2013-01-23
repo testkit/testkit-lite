8c4c3f53490fd6b925f4bbf13d3c25fec05b29bd
Stephane Desneux <stephane.desneux@eurogiciel.fr>
2013-01-23 11:53:04 +0100

    default engine: define a specific return code used in auto test to indicate that test is N/A (retcode 69, as defined in /usr/include/sysexits.h)

--- testkitlite/engines/default/runner.py
+++ testkitlite/engines/default/runner.py
@@ -745,7 +745,10 @@ class TRunner:
                 end_elm.text = datetime.today().strftime("%Y-%m-%d_%H_%M_%S")
                 # set test result
                 if return_code is not None:
-                    if actual_result == "time_out":
+						  # sdx@kooltux.org: if retcode is 69 ("service unavailable" in sysexits.h), test environment is not correct
+                    if actual_result == "69": 
+                        case_result = "N/A"
+                    else actual_result == "time_out":
                         case_result = "BLOCK"
                     else:
                         if expected_result == actual_result:
