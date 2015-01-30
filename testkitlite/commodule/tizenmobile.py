ca9cd823db1b13e07961a1197b77829f965c6fb4
yangx.zhou <yangx.zhou@intel.com>
2015-01-30 13:41:43 +0800

    Modify 'xwalk-launcher' to 'app_launcher -s'

--- testkitlite/commodule/tizenmobile.py
+++ testkitlite/commodule/tizenmobile.py
@@ -61,7 +61,7 @@ WRT_LOCATION = "/home/app/content/tct/opt/%s/%s.wgt"
 # crosswalk constants
 #XWALK_MAIN = "xwalkctl"
 #XWALK_MAIN = "open_app"
-XWALK_MAIN = os.environ.get("Launcher","xwalk-launcher")
+XWALK_MAIN = os.environ.get("Launcher","app_launcher -s")
 if cmp(XWALK_MAIN,'app_launcher') == 0:
     XWALK_MAIN = 'app_launcher -s '
 #XWALK_QUERY_STR = "sdb -s %s shell su - app -c 'export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/5000/dbus/user_bus_socket;ail_list' | grep -w %s | awk '{print $(NF-1)}'"
