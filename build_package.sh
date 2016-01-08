91f2a27a1fc1f5eac2d63a9ccccfffd128a1bd54
stang23 <shaofeng.tang@intel.com>
2016-01-08 14:58:02 +0800

    update travis setting for testkit

--- build_package.sh
+++ build_package.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 dpkg-buildpackage
+chown travis:travis ../testkit-lite_*_all.deb
 ls .. -al
-cp ../testkit-lite_*_all.deb ./dest/
-chown travis:travis ./dest/testkit-lite_*_all.deb
-ls ./dest/ -al
